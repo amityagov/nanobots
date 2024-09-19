b59598043a98d27d7a4b6c31086b43e7f85c78e7
Andrey Mityagov <amityagov@gmail.com>
2024-09-19 19:50:16 +0300

    render one cube

--- crates/ui/src/main.rs
+++ crates/ui/src/main.rs
@@ -1,18 +1,21 @@
-use std::path::PathBuf;
-use bevy::prelude::*;
+mod camera;
+mod model;
 
+use crate::camera::CameraPlugin;
+use crate::model::{LoadModelEvent, ModelPlugin};
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-use bevy::tasks::{AsyncComputeTaskPool, Task};
+use bevy::prelude::*;
 use bevy::window::WindowResolution;
 use bevy_egui::{EguiContexts, EguiPlugin};
-use rfd::FileDialog;
-use futures_lite::future;
+use std::f32::consts::PI;
 
 fn main() {
     App::new()
+        .insert_resource(Msaa::Sample4)
         .add_plugins((
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
+                    title: "Model Viewer".to_owned(),
                     resolution: WindowResolution::new(1920.0, 1080.0),
                     ..default()
                 }),
@@ -22,45 +25,87 @@ fn main() {
             LogDiagnosticsPlugin::default(),
             EguiPlugin,
         ))
-        .add_systems(Startup, setup)
+        .add_plugins((CameraPlugin, ModelPlugin))
         .add_systems(Update, ui_system)
-        .add_systems(Update, poll_model_select_system)
+        .add_systems(Startup, render_cube)
+        .add_systems(Update, render_gizmos)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
-}
-
 #[derive(Component)]
-struct SelectedFile(Task<Option<PathBuf>>);
+struct CubeTemplate(Handle<Mesh>, Handle<StandardMaterial>);
 
-fn ui_system(mut contexts: EguiContexts, mut commands: Commands) {
-    egui::Window::new("").show(contexts.ctx_mut(), |ui| {
-        // load model button
-        if ui.button("Load model").clicked() {
-            let thread_pool = AsyncComputeTaskPool::get();
-            let task = thread_pool.spawn(async move {
-                FileDialog::new()
-                    .add_filter("Model file             ", &["mdl"])
-                    .pick_file()
-            });
-            commands.spawn(SelectedFile(task));
-        }
-    });
+fn render_gizmos(mut gizmos: Gizmos) {
+    gizmos.grid(
+        Vec3::ZERO,
+        Quat::from_rotation_x(PI / 2.),
+        UVec2::splat(40),
+        Vec2::new(1., 1.),
+        LinearRgba::WHITE,
+    );
+    gizmos.grid(
+        Vec3::new(0.0, 20.0, 20.0),
+        Quat::from_rotation_y(PI / 2.),
+        UVec2::splat(40),
+        Vec2::new(1., 1.),
+        LinearRgba::gray(0.5),
+    );
+    gizmos.grid(
+        Vec3::new(20.0, 0.0, 20.0),
+        Quat::from_rotation_z(PI / 2.),
+        UVec2::splat(40),
+        Vec2::new(1., 1.),
+        LinearRgba::WHITE,
+    );
 }
 
-// Poll model file loading
-fn poll_model_select_system(
+fn render_cube(
+    mut ambient_light: ResMut<AmbientLight>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
     mut commands: Commands,
-    mut tasks: Query<(Entity, &mut SelectedFile)>
 ) {
-    for (entity, mut selected_file) in tasks.iter_mut() {
-        if let Some(result) = future::block_on(
-            future::poll_once(&mut selected_file.0)
-        ) {
-            info!("Selected file {:?}", result);
-            commands.entity(entity).remove::<SelectedFile>();
+    ambient_light.brightness = 200.0;
+    ambient_light.color = Color::WHITE;
+
+    let mesh = meshes.add(Cuboid::new(0.98, 0.98, 0.98));
+    let mat = materials.add(StandardMaterial {
+        perceptual_roughness: 0.09,
+        base_color: Color::srgb(0.1, 0.1, 1.0),
+        ..Default::default()
+    });
+
+    commands.spawn(CubeTemplate(mesh.clone(), mat.clone()));
+
+    for x in 0..10 {
+        for y in 0..10 {
+            for z in 0..10 {
+                commands.spawn(PbrBundle {
+                    mesh: mesh.clone_weak(),
+                    material: mat.clone_weak(),
+                    transform: Transform::from_xyz(1.0 * x as f32, 1.0 * y as f32, 1.0 * z as f32),
+                    ..Default::default()
+                });
+            }
         }
     }
+}
+
+fn ui_system(mut contexts: EguiContexts, mut ev_load_model: EventWriter<LoadModelEvent>) {
+    let ctx = contexts.ctx_mut();
+    egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+        egui::menu::bar(ui, |ui| {
+            egui::menu::menu_button(ui, "File", |ui| {
+                if ui.button("Quit").clicked() {
+                    std::process::exit(0);
+                }
+            });
+
+            egui::menu::menu_button(ui, "Model", |ui| {
+                if ui.button("Load model").clicked() {
+                    ev_load_model.send(LoadModelEvent);
+                }
+            });
+        });
+    });
 }
---
+++ crates/ui/src/model.rs
@@ -0,0 +1,104 @@
+use bevy::prelude::*;
+use bevy::tasks::{AsyncComputeTaskPool, Task};
+use futures_lite::future;
+use rfd::FileDialog;
+use std::fs::File;
+use std::io::BufReader;
+use std::time::{Duration, Instant};
+
+#[derive(Event)]
+pub struct LoadModelEvent;
+
+#[derive(Component)]
+struct ModelStatsText;
+
+pub struct ModelPlugin;
+
+impl Plugin for ModelPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<LoadModelEvent>();
+        app.add_systems(Startup, setup);
+        app.add_systems(Update, (listen_load_model_events, poll_model_select));
+    }
+}
+
+struct ModelData {
+    path: String,
+    matrix: mdl::Matrix,
+    elapsed: Duration,
+}
+
+#[derive(Component)]
+struct ModelFileSelectionTask(Task<Option<ModelData>>);
+
+async fn load_matrix() -> anyhow::Result<ModelData> {
+    let filename = FileDialog::new()
+        .add_filter("Model file", &["mdl"])
+        .pick_file();
+
+    if let Some(filename) = filename {
+        let start = Instant::now();
+        let file = File::open(&filename)?;
+        let mut reader = BufReader::new(file);
+        let matrix = mdl::read_matrix(&mut reader)?;
+        let elapsed = start.elapsed();
+        return Ok(ModelData {
+            matrix: matrix,
+            path: filename.to_string_lossy().to_string(),
+            elapsed,
+        });
+    }
+
+    Err(anyhow::anyhow!("No valid filename"))
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        ModelStatsText,
+        TextBundle {
+            text: Text::from_section(
+                "",
+                TextStyle {
+                    font_size: 16.0,
+                    font: asset_server.load("fonts/FiraSans-Regular.ttf"),
+                    ..default()
+                },
+            ),
+            ..default()
+        }
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(5.0),
+            left: Val::Px(15.0),
+            ..default()
+        }),
+    ));
+}
+
+fn listen_load_model_events(mut commands: Commands, mut events: EventReader<LoadModelEvent>) {
+    events.read().for_each(|_| {
+        let thread_pool = AsyncComputeTaskPool::get();
+        let task = thread_pool.spawn(async move { load_matrix().await.ok() });
+        commands.spawn(ModelFileSelectionTask(task));
+    });
+}
+
+fn poll_model_select(
+    mut commands: Commands,
+    mut tasks: Query<(Entity, &mut ModelFileSelectionTask)>,
+    mut model_stat_text: Query<&mut Text, With<ModelStatsText>>,
+) {
+    for (entity, mut selected_file) in tasks.iter_mut() {
+        if let Some(result) = future::block_on(future::poll_once(&mut selected_file.0)) {
+            if let Some(data) = result {
+                commands.entity(entity).remove::<ModelFileSelectionTask>();
+                for mut text in &mut model_stat_text {
+                    text.sections[0].value = format!(
+                        "Model {}, R: {}, loaded in {:?}",
+                        data.path, data.matrix.r, data.elapsed
+                    );
+                }
+            }
+        }
+    }
+}
